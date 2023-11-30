e97b22bf78eea77e7ce540271047a9c8b92ddf8d
Gaby Matinez <g4bmz02@gmail.com>
2023-11-30 20:52:53 -0300

    commit

--- script.js
+++ script.js
@@ -6,8 +6,23 @@ const inputContainer = document.querySelector(".input-container");
 // INPUT
 const input = document.querySelector("#agent-name");
 
-// CONTAINER DE TODA LA INFO DE AGENTE
-const infoContainer = document.querySelector(".web-agent-content-container");
+// CONTAINER DE NOMBRE Y DESCRIPCION
+const nameDescContainer = document.querySelector(".agent-name-desc-container");
+
+// CONTAINER DE NOMBRE DE HABILIDAD Y DESCRIPCION DE HABILIDAD
+const abilityDescContainer = document.querySelector(
+  ".agent-ability-desc-container"
+);
+
+// CONTAINER DE SELECTOR DE HABILIDAD
+const abilitySelectorContainer = document.querySelector(
+  ".agent-ability-changer-container"
+);
+
+// CONTAINER DE RETRATO
+const agentPortrairContainer = document.querySelector(
+  ".web-agent-img-container"
+);
 
 async function searchAgent(e) {
   e.preventDefault();
@@ -15,103 +30,91 @@ async function searchAgent(e) {
   agentSelected = await agentData.find(
     (item) => input.value === item.displayName
   );
-//   abilitySelected = agentSelected.trim("");
-  console.log(agentSelected)
-  agentTemplate(agentSelected);
-  // infoContainer.innerHTML = agentTemplate(agentSelected)
+  agentAbilities = agentSelected.abilities;
+  abilityIcon = agentAbilities.map((item) => item.displayIcon)
+
+  nameDescContainer.innerHTML = agentTemplateNameAndDesc(agentSelected);
+  // abilityDescContainer.innerHTML = agentTemplateAbilitieAndDesc(agentAbilities);
+  abilitySelectorContainer.innerHTML = agentTemplateAbilitySelector(abilityIcon);
+  // agentPortrairContainer.innerHTML = agentTemplatePortrait(agent);
 }
 
-function agentTemplate(data) {
-    const {displayName, fullPortrait, } = data
+function agentTemplateNameAndDesc(data) {
+  const { displayName, description } = data;
 
-    console.log(data)
-    return `
-    <!-- CONTAINER DEL LADO IZQUIERDO (PC) O LADO DE ABAJO(MOVIL) -->
-          <div class="web-agent-info-container">
-            <!-- CONTAINER DE DATOS DE PERSONAJE -->
-            <div class="agent-text-container">
-  
-            <!-- CONTAINER DE NOMBRE Y DESCRIPCION DE PERSONAJE -->
-            <div class="agent-name-desc-container">
-  
-            <!-- NOMBRE DE PERSONAJE -->
-            <h3></h3>
-  
-            <!-- DESCRIPCION DE PERSONAJE -->
-            <p>
-                              “The bionic Swede Breach fires powerful, targeted kinetic blasts to aggressively clear a
-                              path
-                              through enemy ground. The damage and disruption he inflicts ensures no fight is ever fair.”
-              </p>
-  
-            <!-- </div> -->
-  
-            <!-- CONTAINER DE NOMBRE Y DESCRIPCION DE HABILIDAD DE PERSONAJE -->
-            <div class="agent-ability-desc-container">
-  
-            <!-- NOMBRE DE HABILIDAD DE PERSONAJE -->
-            <h3>FLASHPOINT</h3>
-  
-            <!-- DESCRIPCION DE HABILIDAD DE PERSONAJE -->
-            <p>
-                              EQUIP a Blinding charge. FIRE the charge to set a fast-acting burst through the wall. The
-                              charge
-                              detonates to Blind all players looking at it.
-                          </p>
-  
-            </div>
-  
-            </div>
-  
-            <!-- CONTAINER DE CAMBIADOR DE HABILIDADES DE PERSONAJE (SE CAMBIA CON UN CLICK EN CADA ICONO) -->
-            <div class="agent-ability-changer-container">
-  
-            <!-- CONTAINER DE HABILIDAD 1 -->
-            <div class="agent-ability-1 ability">
-  
-            <!-- IMAGEN DE HABILIDAD -->
-            <img src="/img/Ability-1-icon.svg" alt="">
-  
-            </div>
-  
-            <!-- CONTAINER DE HABILIDAD 2 -->
-            <div class="agent-ability-2 ability">
-  
-            <!-- IMAGEN DE HABILIDAD -->
-            <img src="/img/Ability-2-icon.svg" alt="">
-  
-            </div>
-  
-            <!-- CONTAINER DE HABILIDAD 3 (GRANADA) -->
-            <div class="agent-ability-3 ability">
-  
-            <!-- IMAGEN DE HABILIDAD -->
-            <img src="/img/Grenade-icon.svg" alt="">
-  
-            </div>
-  
-            <!-- CONTAINER DE HABILIDAD 4 (ULTIMATE) -->
-            <div class="agent-ability-4 ability">
-  
-            <!-- IMAGEN DE HABILIDAD -->
-            <img src="/img/Ultimate-icon.svg" alt="">
-  
-            </div>
-            </div>
-          </div>
-  
-          <!-- CONTAINER DE LADO DERECHO (PC) O LADO DE ABAJO (MOVIL) -->
-          <div class="web-agent-img-container">
-  
-          <!-- IMAGEN DE PERSONAJE -->
-          <div class="agent-img"></div>
-  
-          <!-- TEXTO DE FONDO DE LA IMAGEN -->
-          <p>BREACH</p>
-  
-          </div>
+  return `
+    <!-- NOMBRE DE PERSONAJE -->
+    <h3>${displayName}</h3>
+
+    <!-- DESCRIPCION DE PERSONAJE -->
+    <p>
+      “${description}”
+    </p>
     `;
+}
 
+function agentTemplateAbilitieAndDesc(data) {
+  // const { displayName, description} = data;
+
+  return `
+    <!-- NOMBRE DE HABILIDAD DE PERSONAJE -->
+    <h3></h3>
+
+    <!-- DESCRIPCION DE HABILIDAD DE PERSONAJE -->
+    <p>
+      
+    </p>
+  `;
+}
+
+function agentTemplateAbilitySelector(data) {
+  
+  return `
+  <img src=${data[0]} alt="" data-id="Ability1">
+    <!-- CONTAINER DE HABILIDAD 1 -->
+    <div class="agent-ability-1 ability">
+
+      <!-- IMAGEN DE HABILIDAD -->
+      <img src=${data[0]} alt="" data-id="Ability1">
+
+    </div>
+
+    <!-- CONTAINER DE HABILIDAD 2 -->
+    <div class="agent-ability-2 ability">
+
+      <!-- IMAGEN DE HABILIDAD -->
+      <img src=${data[1]} alt="" data-id="Ability2">
+
+    </div>
+
+    <!-- CONTAINER DE HABILIDAD 3 (GRANADA) -->
+    <div class="agent-ability-3 ability">
+
+      <!-- IMAGEN DE HABILIDAD -->
+      <img src=${data[2]} alt="" data-id="Grenade">
+
+    </div>
+
+    <!-- CONTAINER DE HABILIDAD 4 (ULTIMATE) -->
+    <div class="agent-ability-4 ability">
+
+      <!-- IMAGEN DE HABILIDAD -->
+      <img src=${data[3]} alt="" data-id="Ultimate">
+
+    </div>
+  `;
+}
+
+function agentTemplatePortrait(data) {
+
+
+  return `
+  <!-- IMAGEN DE PERSONAJE -->
+        <div class="agent-img"></div>
+
+    <!-- TEXTO DE FONDO DE LA IMAGEN -->
+    <p>BREACH</p>
+  `;
 }
 
 function init() {
